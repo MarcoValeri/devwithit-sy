f9794983e73c0e9d695cf15b0727703b7a934881
Marco Valeri <marcovaleri@hotmail.it>
2023-04-18 10:15:55 +0100

    Add single article controller

---
+++ src/Controller/GuideController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GuideRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GuideController extends AbstractController
+{
+    #[Route('/guide/{slug}', name: 'app_guide')]
+    public function guide(GuideRepository $guideRepository, string $slug)
+    {
+        $guide = $guideRepository->findOneBy(['url' => $slug]);
+
+        return $this->render("guides/guide.html.twig", [
+            'guide'    => $guide,
+            'slug'      => $slug
+        ]);
+    }
+}
